2bd336677c0d0164680a9fb20bf84cbe2f6ec614
jichun <975171015@qq.com>
2020-09-08 00:24:05 +0800

    Initial commit

---
+++ src/main/java/com/jichun/gitTest/test01.java
@@ -0,0 +1,11 @@
+package com.jichun.gitTest;
+
+/**
+ * @author jichun
+ * @create 2020-09-08 0:09
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("test01");
+    }
+}
